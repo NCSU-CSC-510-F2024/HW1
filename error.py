12b3aec295519db247960ea8b6904800a24449c0
NicoField <74797059+NicoField@users.noreply.github.com>
2024-08-22 17:11:30 +0000

    adding error.py and License.md

---
+++ error.py
@@ -0,0 +1,11 @@
+def error():
+    x = 1
+    y = 2
+    z = x[3] + y
+    print("line 5")
+    print("line 6")
+    print("line 7")
+    print("line 8")
+    print("line 9")
+    print("line 10")
+error()
