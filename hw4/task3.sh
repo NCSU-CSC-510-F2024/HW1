bc2a17164425535c475a10e881293775e0405c90
Riley Joncas <rbjoncas@ncsu.edu>
2024-10-01 13:02:51 -0400

    task shell files for each part

---
+++ hw4/task3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+passengers=`gawk -F, '$3 == 2 { print }' titanic.csv | sed -e 's/female/F/g' -e 's/male/M/g' ` 
+
+echo "$passengers"
+
+echo ""
+
+gawk -F, '$3 == 2 { print }' titanic.csv | sed -e 's/female/F/g' -e 's/male/M/g' | gawk -F, 'BEGIN { sum=0; count=0 } $7 != "" && $7 ~ /^[0-9]+(\.[0-9]+)?$/ { sum += $7; count++ } END { if (count > 0) print "Average age: " sum/count; else print "Average age could not be computed" }'
