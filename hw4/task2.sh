8fe81944c9884221b458a535836e2f090a76e81f
NicoField <74797059+NicoField@users.noreply.github.com>
2024-10-01 17:50:18 +0000

    updating file path for task2.sh

--- hw4/task2.sh
+++ hw4/task2.sh
@@ -1,1 +1,3 @@
-grep -l "sample" file* | xargs grep -o "CSC510" | uniq -c | grep -E '^\s*([3-9]|[1-9][0-9]+)' | gawk '{gsub(/:CSC510/, "", $2); print $1, $2}' | while read count file; do echo "$count $file $(stat -c%s "$file")"; done | sort -k1,1nr -k3,3nr | sed 's/file_/filtered_/'
+#!/bin/bash
+
+grep -l "sample" dataset1/file* | xargs grep -o "CSC510" | uniq -c | grep -E '^\s*([3-9]|[1-9][0-9]+)' | gawk '{gsub(/:CSC510/, "", $2); print $1, $2}' | while read count file; do echo "$count $file $(stat -c%s "$file")"; done | sort -k1,1nr -k3,3nr | sed 's/file_/filtered_/'
